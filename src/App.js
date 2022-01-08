d27c92b218e33a940e8fcba23209bab9f3df8a9d
Kay <drumer19861018@gmail.com>
2022-01-08 11:37:38 +1100

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+// ブートストラップを輸入
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import './App.scss';
+
+
 
 function App() {
   return (
-<h2>dfakfa</h2>
+<Navbar />
   );
 }
 
