ecbb30d90c6fec76139b017a9bc48e4b92743d8a
Kay <drumer19861018@gmail.com>
2022-01-08 23:28:05 +1100

    About Me Completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Typed from "react-typed";// Library
-
+// import { ParticleBg } from "./ParticleBg";
 
 
 export const Header = () => {
   return (
       <div className="header-wrapper">
+             {/* <ParticleBg /> */}
         <div className="main-info">
           <h1 className="main-infoText">web development and websites promotions</h1>
 
---
+++ src/components/ParticleBg.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import Particles from "react-tsparticles";
+
+export const ParticleBg = () => {
+  return (
+    <Particles
+        className="particles__canvas"
+        // params={{
+        //   particles: {
+        //     number: {
+        //       value: 30,
+        //       density: {
+        //         enable: true,
+        //         value_area: 900,
+        //       },
+        //     },
+        //     shape: {
+        //       type: "circle",
+        //       stroke: {
+        //         width: 6,
+        //         color: "#f9ab00",
+        //       },
+        //     },
+        //   },
+        // }}
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color:"#f9ab00"
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#f9ab00",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+  )
+}
+
+export default ParticleBg
